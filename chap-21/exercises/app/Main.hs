ee98155db43d51ec5c8957fd01a79edbdd14341a
Ali Ahmed <ali.ahmed@punchcard.io>
2019-09-14 23:51:44 -0600

    chap 21 last exercise

--- chap-21/exercises/app/Main.hs
+++ chap-21/exercises/app/Main.hs
@@ -283,44 +283,44 @@ threeVar = Three "abc" (Sum 2) (2, "xyz", [Just 1, Just 2])
 
 -------------------------------------------------------------
 
-data Pair a b
-  = Pair a b deriving (Eq, Show)
+-- data Pair a b
+--   = Pair a b deriving (Eq, Show)
 
-instance (Semigroup a, Semigroup b) => Semigroup (Pair a b) where
-  (<>) (Pair a1 b1) (Pair a2 b2) = Pair (a1 <> a2) (b1 <> b2)
+-- instance (Semigroup a, Semigroup b) => Semigroup (Pair a b) where
+--   (<>) (Pair a1 b1) (Pair a2 b2) = Pair (a1 <> a2) (b1 <> b2)
 
-instance (Monoid a, Monoid b) => Monoid (Pair a b) where
-  mempty = Pair mempty mempty
+-- instance (Monoid a, Monoid b) => Monoid (Pair a b) where
+--   mempty = Pair mempty mempty
 
-instance Functor (Pair a) where
-  fmap f (Pair a b) = Pair a (f b)
+-- instance Functor (Pair a) where
+--   fmap f (Pair a b) = Pair a (f b)
 
-instance Monoid a => Applicative (Pair a) where
-  pure = Pair mempty
-  (<*>) (Pair a1 f) (Pair a2 x) = Pair (a1 <> a2) (f x)
+-- instance Monoid a => Applicative (Pair a) where
+--   pure = Pair mempty
+--   (<*>) (Pair a1 f) (Pair a2 x) = Pair (a1 <> a2) (f x)
 
-instance Monoid a => Monad (Pair a) where
-  return = pure
-  (Pair a1 b1) >>= f = Pair (a1 <> a2) b2
-    where Pair a2 b2 = f b1
+-- instance Monoid a => Monad (Pair a) where
+--   return = pure
+--   (Pair a1 b1) >>= f = Pair (a1 <> a2) b2
+--     where Pair a2 b2 = f b1
 
-instance Foldable (Pair a) where
-  -- foldMap f (Pair a b) = f b
-  foldr f z (Pair a b) = f b z
+-- instance Foldable (Pair a) where
+--   -- foldMap f (Pair a b) = f b
+--   foldr f z (Pair a b) = f b z
 
-instance Traversable (Pair a) where
-  traverse f (Pair a b) = Pair a <$> f b
+-- instance Traversable (Pair a) where
+--   traverse f (Pair a b) = Pair a <$> f b
 
-instance (Arbitrary a, Arbitrary b) => Arbitrary (Pair a b) where
-  arbitrary = Pair <$> arbitrary <*> arbitrary
+-- instance (Arbitrary a, Arbitrary b) => Arbitrary (Pair a b) where
+--   arbitrary = Pair <$> arbitrary <*> arbitrary
 
-instance (Eq a, Eq b) => EqProp (Pair a b) where
-  (=-=) = eq
+-- instance (Eq a, Eq b) => EqProp (Pair a b) where
+--   (=-=) = eq
 
-type PairType = Pair (Sum Int) (Int, String, [Int])
+-- type PairType = Pair (Sum Int) (Int, String, [Int])
 
-pairVar :: PairType
-pairVar = Pair (Sum 2) (2, "abc", [1, 2, 3])
+-- pairVar :: PairType
+-- pairVar = Pair (Sum 2) (2, "abc", [1, 2, 3])
 
 -- main :: IO ()
 -- main = do
@@ -352,13 +352,14 @@ instance (Monoid a) => Applicative (Big a) where
 
 instance (Monoid a) => Monad (Big a) where
   return = pure
-  (Big a1 b1 b2) >>= f = Big (a1 <> a2) b3 b4
+  (Big a1 b1 b2) >>= f = Big (a1 <> a2 <> a3) b3 b5
     where
       Big a2 b3 b4 = f b1
+      Big a3 b5 b6 = f b2
 
 instance Foldable (Big a) where
   -- foldMap f (Big a b1 b2) = f b1 <> f b2
-  foldr f z (Big a b1 b2) = f (b1 <> b2) z
+  foldr f z (Big a b1 b2) = (f b1 . f b2) z
 
 instance Traversable (Big a) where
   traverse f (Big a b1 b2) = Big a <$> (f b1) <*> (f b2)
@@ -375,9 +376,180 @@ type BigType = Big (Sum Int) (Int, String, [Int])
 bigVar :: BigType
 bigVar = Big (Sum 2) (2, "abc", [1, 2, 3]) (3, "xyz", [4, 5, 6])
 
+-- main :: IO ()
+-- main = do
+--   quickBatch $ functor bigVar
+--   quickBatch $ applicative bigVar
+--   quickBatch $ monad bigVar
+--   quickBatch $ traversable bigVar
+
+----------------------------------------------------------------------
+
+-- data MP f g a
+--   = Pair (f a) (g a) deriving (Eq, Show)
+
+-- instance (Semigroup (f a), Semigroup (g a)) => Semigroup (MP f g a) where
+--   (<>) (Pair f1 g1) (Pair f2 g2) = Pair (f1 <> f2) (g1 <> g2)
+
+-- instance (Monoid (f a), Monoid (g a)) => Monoid (MP f g a) where
+--   mempty = Pair mempty mempty
+
+-- instance (Functor f, Functor g) => Functor (MP f g) where
+--   fmap n (Pair f g) = Pair (n <$> f) (n <$> g)
+
+-- instance (Applicative f, Applicative g) => Applicative (MP f g) where
+--   pure x = Pair (pure x) (pure x)
+--   (<*>) (Pair f1 g1) (Pair x1 x2) = Pair (f1 <*> x1) (g1 <*> x2)
+
+-- instance (Monad f, Monad g) => Monad (MP f g) where
+--   return = pure
+--   Pair f g >>= m = Pair (f1 <> f2) (g1 <> g2)
+--     where
+--       Pair f1 g1 = m f
+--       Pair f2 g2 = m g
+
+-- instance (Arbitrary (f a), Arbitrary (g a)) => Arbitrary (MP f g a) where
+--   arbitrary = do
+--     f <- arbitrary
+--     g <- arbitrary
+--     return $ Pair f g
+
+-- instance (Eq (f a), Eq (g a)) => EqProp (MP f g a) where
+--   (=-=) = eq
+
+-- type MPType = MP Maybe Maybe (Int, String, [Int])
+
+-- mpVar :: MPType
+-- mpVar = Pair (Just (1, "abc", [1, 2, 3])) (Just (1, "xyz", [3, 4, 5]))
+
+-- main :: IO ()
+-- main = do
+--   quickBatch $ functor mpVar
+--   quickBatch $ applicative mpVar
+  -- quickBatch $ monad mpVar
+
+--------------------------------------------------------
+
+data Bigger a b
+  = Bigger a b b b deriving (Eq, Show)
+
+instance (Semigroup a, Semigroup b) => Semigroup (Bigger a b) where
+  (<>) (Bigger a1 b1 b2 b3) (Bigger a2 c1 c2 c3) =
+      Bigger (a1 <> a2) (b1 <> c1) (b2 <> c2) (b3 <> c3)
+
+instance (Monoid a, Monoid b) => Monoid (Bigger a b) where
+  mempty = Bigger mempty mempty mempty mempty
+
+instance Functor (Bigger a) where
+  fmap f (Bigger a b1 b2 b3) = Bigger a (f b1) (f b2) (f b3)
+
+instance Foldable (Bigger a) where
+  foldMap f (Bigger a b1 b2 b3) = f b1 <> f b2 <> f b3
+
+instance Traversable (Bigger a) where
+  traverse f (Bigger a b1 b2 b3) = Bigger a <$> f b1 <*> f b2 <*> f b3
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Bigger a b) where
+  arbitrary = Bigger <$> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary
+
+instance (Eq a, Eq b) => EqProp (Bigger a b) where
+  (=-=) = eq
+
+type BiggerType = Bigger (Sum Int) (Int, String, [Int])
+
+biggerVar :: BiggerType
+biggerVar = Bigger (Sum 2) (2, "abc", [1, 2, 3]) (3, "xyz", [4, 5, 6]) (4, "ghi", [7, 8, 9])
+
+-- main :: IO ()
+-- main = do
+--   quickBatch $ functor biggerVar
+--   quickBatch $ traversable biggerVar
+
+---------------------------------------------------
+
+
+data S n a
+  = S (n a) a deriving (Eq, Show)
+
+instance (Semigroup a, Semigroup (n a)) => Semigroup (S n a) where
+  (<>) (S na1 a1) (S na2 a2) = S (na1 <> na1) (a1 <> a2)
+
+instance (Monoid a, Monoid (n a)) => Monoid (S n a) where
+  mempty = S mempty mempty
+
+instance (Functor n) => Functor (S n) where
+  fmap f (S na a) = S (fmap f na) (f a)
+
+instance Foldable n => Foldable (S n) where
+  foldMap f (S na a) = foldMap f na <> f a
+
+instance Traversable n => Traversable (S n) where
+  traverse f (S na a) = S <$> (traverse f na) <*> f a
+
+instance ( Functor n
+          , Arbitrary (n a)
+          , Arbitrary a )
+  => Arbitrary (S n a) where
+    arbitrary =
+      S <$> arbitrary <*> arbitrary
+
+instance ( Applicative n
+          , Testable (n Property)
+          , Eq a
+          , Eq (n a)
+          , EqProp a)
+  => EqProp (S n a) where
+    (=-=) = eq
+
+type TypeS = S [] (Int, String, [Int])
+
+varS :: TypeS
+varS = S [(1, "abc", [1, 2, 3])] (2, "yth", [4, 5, 6])
+
+-- main :: IO ()
+-- main =
+--   quickBatch $ traversable biggerVar
+
+-----------------------------------------------------------
+
+data Tree a
+  = Empty
+  | Leaf a
+  | Node (Tree a) a (Tree a)
+  deriving (Eq, Show)
+
+instance (Semigroup a) => Semigroup (Tree a) where
+  (<>) Empty x = x
+  (<>) x Empty = x
+  (<>) (Node l1 a1 r1) (Node l2 a2 r2) = Node (l1 <> l2) (a1 <> a2) (r1 <> r2)
+
+instance Monoid a => Monoid (Tree a) where
+  mempty = Empty
+
+instance Functor Tree where
+  fmap _ Empty = Empty
+  fmap f (Node left v right) = Node (fmap f left) (f v) (fmap f right)
+
+instance Foldable Tree where
+  foldMap f Empty = mempty
+  foldMap f (Node left v right) = (foldMap f left) <> f v <> (foldMap f right)
+
+instance Traversable Tree where
+  traverse f Empty = pure Empty
+  traverse f (Node left v right) = Node <$> traverse f left <*> f v <*> traverse f right
+
+instance Arbitrary a => Arbitrary (Tree a) where
+  arbitrary = Node <$> arbitrary <*> arbitrary <*> arbitrary
+
+instance Eq a => EqProp (Tree a) where
+  (=-=) = eq
+
+type TreeType = Tree (Int, String, [Int])
+
+varTree :: TreeType
+varTree = Node Empty (1, "abc", [1, 2, 3]) Empty
+
 main :: IO ()
 main = do
-  quickBatch $ functor bigVar
-  quickBatch $ applicative bigVar
-  quickBatch $ monad bigVar
-  quickBatch $ traversable bigVar
+  -- quickBatch $ functor varTree
+  quickBatch $ traversable varTree
