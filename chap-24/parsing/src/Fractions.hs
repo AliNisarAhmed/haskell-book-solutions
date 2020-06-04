19ad6cb5d3e5bb3cfec37d9520df26441ba4fab4
Ali Ahmed <ali.ahmed@punchcard.io>
2020-06-04 00:03:02 -0600

    parsing ini files

--- chap-24/parsing/src/Fractions.hs
+++ chap-24/parsing/src/Fractions.hs
@@ -1,10 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Fractions where 
+module Fractions where
 
-import Text.Trifecta
-import Control.Applicative
-import Data.Ratio ((%))
+import           Control.Applicative
+import           Data.Ratio          ((%))
+import           Text.Trifecta
 
 badFraction = "1/0"
 alsoBad = "10"
@@ -12,22 +12,30 @@ shouldWork = "1/2"
 shouldAlsoWork = "2/1"
 
 parseFraction :: Parser Rational
-parseFraction = do 
+parseFraction = do
   numerator <- decimal
   char '/'
   denominator <- decimal
-  case denominator of 
+  case denominator of
     0 -> fail "Denominator cannot be zero"
     _ -> return (numerator % denominator)
 
--- parseFull :: Parser Integer 
+-- parseFull :: Parser Integer
 -- parseFull = sliced .
 
 main :: IO ()
-main = do 
+main = do
   let pf = parseString parseFraction mempty
   print $ pf shouldWork
   print $ pf shouldAlsoWork
 
   print $ pf badFraction
-  print $ pf alsoBad 
+  print $ pf alsoBad
+
+---
+
+type NumberOrFraction = Either Double Rational
+
+parseNumberOrFraction :: Parser NumberOrFraction
+parseNumberOrFraction =
+  (Left <$> try double) <|> (Right <$> try parseFraction)
